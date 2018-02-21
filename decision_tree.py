00ae62301d4e27b93a04f2b45d62b1d8c7f8e3d2
Bo Peng <xbopeng@outlook.com>
2018-02-21 12:20:05 -0600

    Add comments for function

--- decision_tree.py
+++ decision_tree.py
@@ -8,6 +8,13 @@ Build a ID3 like decision tree
 """
 
 def makeSubtree(instance_data_trn, var_name_tree, var_in_tree, var_val_cur):
-
+    """
+    Build the decision tree
+    :param instance_data_trn: training data, including multiple instances
+    :param var_name_tree:
+    :param var_in_tree: variable already put as a node in the tree
+    :param var_val_cur:
+    :return: decision_tree_ID3:
+    """
     decision_tree_ID3 = 47
     return decision_tree_ID3
